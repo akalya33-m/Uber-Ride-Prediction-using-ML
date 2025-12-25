9634a6046f606e1e8e08dad763b118c0161a2daa
demo <akalya8119@gmail.com>
2025-12-25 12:04:15 +0530

    Add requirements.txt and deployment fixes

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def predict():
 
    return render_template('index.html',prediction_text='Number of Weekly Rides is  {}'.format(math.floor(output)))
 
-if __name__ =="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
